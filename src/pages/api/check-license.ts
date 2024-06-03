70c1e5eca6dd64c61ed1a016c421f7073a809712
Sean Cooper <scooper@adaptavist.com>
2024-06-03 13:28:52 +0100

    first commit

---
+++ src/pages/api/check-license.ts
@@ -0,0 +1,38 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import {getAuth} from "@clerk/nextjs/server";
+import fetch from 'node-fetch';
+
+type License = { capabilities: string[] }
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.method !== "GET") return res.status(405).end();
+
+    if (!process.env.SALABLE_PRODUCT_UUID) return res.status(500).json({
+        error: 'Internal Server Error: Missing salable product UUID'
+    });
+    if (!process.env.SALABLE_READ_LICENSE) return res.status(500).json({
+        error: 'Internal Server Error: Missing read license api key'
+    });
+
+    const {userId} = getAuth(req);
+
+    if(!userId) return res.status(401).json({error: 'Unauthenticated'});
+
+    try {
+        const response = await fetch(
+            `https://api.salable.app/licenses/check?productUuid=${process.env.SALABLE_PRODUCT_UUID}&granteeIds=${userId}`, {
+                headers: {
+                    'x-api-key': process.env.SALABLE_READ_LICENSE,
+                    'version': 'v2'
+                }
+            });
+        const license = await response.json() as License;
+        return res.status(200).json({capabilities: license.capabilities});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({error: 'Failed to check license'});
+    }
+}
